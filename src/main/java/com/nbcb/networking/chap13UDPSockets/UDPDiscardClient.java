1347014c677ca42f3346f53395a66253bc7d3725
zhoushuo19850509 <zhoushuo19850509@163.com>
2021-03-20 16:27:53 +0800

    commit UDPDiscardClient/UDPDiscardServer

--- src/main/java/com/nbcb/networking/chap13UDPSockets/UDPDiscardClient.java
+++ src/main/java/com/nbcb/networking/chap13UDPSockets/UDPDiscardClient.java
@@ -1,4 +1,73 @@
 package com.nbcb.networking.chap13UDPSockets;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class UDPDiscardClient {
+    public static final int DEFAULT_PORT = 8099;
+    public static final String DEFAULT_SERVER = "localhost";
+
+    public static void main(String[] args) {
+        System.out.println("start client!");
+        /**
+         * host
+         */
+        String hostName = DEFAULT_SERVER;
+
+        /**
+         * port
+         */
+        int port = DEFAULT_PORT;
+
+
+        try {
+
+            InetAddress server = InetAddress.getByName(hostName);
+            /**
+             * Datagram socket
+             */
+            DatagramSocket theSocket = new DatagramSocket();
+
+            /**
+             * Read from user input
+             */
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+
+            /**
+             * send data from input to the server continuously
+             */
+            while(true){
+
+
+                String readLine = userInput.readLine();
+                byte[] data = readLine.getBytes("UTF-8");
+
+                DatagramPacket packet =
+                        new DatagramPacket(data, data.length, server, port);
+
+                theSocket.send(packet);
+
+            }
+
+            /**
+             * send sth to the server
+             */
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
--- src/main/java/com/nbcb/networking/chap13UDPSockets/UDPDiscardServer.java
+++ src/main/java/com/nbcb/networking/chap13UDPSockets/UDPDiscardServer.java
@@ -1,4 +1,63 @@
 package com.nbcb.networking.chap13UDPSockets;
 
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class UDPDiscardServer {
+
+    public static final int DEFAULT_PORT = 8099;
+    public static final int MAX_PACKET_SIZE = 8099;
+
+    public static void main(String[] args) {
+        System.out.println("start server");
+
+
+        int port = DEFAULT_PORT;
+        byte[] buffer = new byte[MAX_PACKET_SIZE];
+
+
+        try {
+            /**
+             * Datagram socket
+             */
+            DatagramSocket server = new DatagramSocket(port);
+            System.out.println("the server start listening on port: " + port);
+
+            /**
+             * Datagram packet
+             */
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+
+            /**
+             * the server receive the content from the client(s)
+             */
+            while(true){
+
+                server.receive(packet);
+
+                /**
+                 * translate the packet to String
+                 */
+                String str = new String(packet.getData(), 0 , packet.getLength(), "UTF-8");
+                System.out.println(packet.getAddress() + " on port: " + port + " says: " + str );
+                packet.setLength(buffer.length);
+
+            }
+
+
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 }

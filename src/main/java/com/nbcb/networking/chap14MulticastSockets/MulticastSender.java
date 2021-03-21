aee40c6db02df71c914cdbb8bc24c0353c0d6a7b
zhoushuo19850509 <zhoushuo19850509@163.com>
2021-03-21 21:00:06 +0800

    chap14 MulticastSocket

--- src/main/java/com/nbcb/networking/chap14MulticastSockets/MulticastSender.java
+++ src/main/java/com/nbcb/networking/chap14MulticastSockets/MulticastSender.java
@@ -1,4 +1,66 @@
 package com.nbcb.networking.chap14MulticastSockets;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
 public class MulticastSender {
+    public static void main(String[] args) {
+
+        /**
+         * address/port
+         */
+        String hostName = "all-systems.mcast.net";
+        InetAddress ia = null;
+        int port = 4000;
+        try {
+            ia = InetAddress.getByName(hostName);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        byte ttl = (byte)1;
+
+        /**
+         * data and datagram packet
+         */
+        byte[] data = "Here's something I want to say".getBytes();
+        DatagramPacket packet = new DatagramPacket(data, data.length, ia, port);
+
+
+        /**
+         * multicast socket and join the group
+         */
+        MulticastSocket socket = null;
+        try {
+            socket = new MulticastSocket();
+            socket.joinGroup(ia);
+
+            /**
+             * send msg to the group
+             */
+            for (int i = 0; i < 10; i++) {
+                socket.send(packet,ttl);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.leaveGroup(ia);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(null != socket){
+                socket.close();
+            }
+        }
+
+
+
+
+    }
 }
--- src/main/java/com/nbcb/networking/chap14MulticastSockets/MulticastSniffer.java
+++ src/main/java/com/nbcb/networking/chap14MulticastSockets/MulticastSniffer.java
@@ -1,4 +1,51 @@
 package com.nbcb.networking.chap14MulticastSockets;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
 public class MulticastSniffer {
+    public static void main(String[] args) {
+
+        /**
+         * (group)address and port
+         */
+        String groupName = "all-systems.mcast.net";
+        InetAddress ia = null;
+        int port = 4000;
+        try {
+            ia = InetAddress.getByName(groupName);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+
+        /**
+         * multicast socket and join the group
+         */
+        MulticastSocket socket = null;
+        try {
+            socket = new MulticastSocket(port);
+            socket.joinGroup(ia);
+
+            /**
+             * receive the datagram packet from the group continuously
+             */
+            while(true){
+                byte[] buffer = new byte[100];
+                DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
+
+                socket.receive(dp);
+                String s = new String(dp.getData());
+                System.out.println("msg from group : [" + s + "]");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
 }
